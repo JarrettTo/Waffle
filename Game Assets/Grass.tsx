a46aaa98d71e994ae3a525639886523d0b715f8f
jerseyy2 <jers_to@dlsu.edu.ph>
2024-02-12 02:11:44 +0800

    Asset generation and setup

---
+++ Game Assets/Grass.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Grass" tilewidth="12" tileheight="12" tilecount="126" columns="14">
+ <image source="Sprout Lands - Sprites - Basic pack/Tilesets/Grass.png" width="176" height="112"/>
+</tileset>
